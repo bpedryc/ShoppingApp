cbff0482e860113721573cb43ab2b7643e9ff035
Bartek Pedryc <b.pedryc11@gmail.com>
2021-11-07 13:46:05 +0100

    Implement proper title display on each fragment
    Titles are displayed based on Android Jetpack Navigation Components

--- app/src/main/java/com/haxos/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/haxos/shoppingapp/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.haxos.shoppingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import com.google.android.material.navigation.NavigationView
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
     }
 }
