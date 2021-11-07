819afbf4da61843bc64afd239499d6ea1cdaba29
Bartek Pedryc <b.pedryc11@gmail.com>
2021-11-07 22:35:16 +0100

    Make it impossible to modify archived shopping list

--- app/src/main/java/com/haxos/shoppingapp/shoppinglistdetails/ShoppingListDetailsViewModel.kt
+++ app/src/main/java/com/haxos/shoppingapp/shoppinglistdetails/ShoppingListDetailsViewModel.kt
@@ -3,19 +3,26 @@ package com.haxos.shoppingapp.shoppinglistdetails
 import androidx.lifecycle.*
 import com.haxos.shoppingapp.data.Grocery
 import com.haxos.shoppingapp.data.Result
+import com.haxos.shoppingapp.data.ShoppingList
 import com.haxos.shoppingapp.data.database.GroceryDao
+import com.haxos.shoppingapp.data.database.ShoppingListDao
 import com.haxos.shoppingapp.utils.Event
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ShoppingListDetailsViewModel @Inject constructor(
+    private val shoppingListDao: ShoppingListDao,
     private val groceryDao: GroceryDao
 ) : ViewModel() {
 
     private val _groceries = MutableLiveData<List<Grocery>>().apply { value = emptyList() }
     val groceries: LiveData<List<Grocery>> = _groceries
+
+    private val _archived = MutableLiveData<Boolean>()
+    val archived: LiveData<Boolean> = _archived
 
     private val _createdGroceryEvent = MutableLiveData<Event<Unit>>()
     val createdGroceryEvent: LiveData<Event<Unit>> = _createdGroceryEvent
@@ -28,8 +35,14 @@ class ShoppingListDetailsViewModel @Inject constructor(
     }
 
     fun start(shoppingListId: String) = viewModelScope.launch {
-        val groceries = getGroceries(shoppingListId) ?: return@launch
+        val groceriesResult = async { fetchGroceries(shoppingListId) }
+        val shoppingListResult = async { fetchShoppingList(shoppingListId) }
+
+        val groceries = handleResult(groceriesResult.await()) ?: return@launch
+        val shoppingList = handleResult(shoppingListResult.await()) ?:return@launch
+
         _groceries.value = groceries
+        _archived.value = shoppingList.archived
     }
 
     fun createGrocery(shoppingListId: String, groceryName: String) = viewModelScope.launch {
@@ -43,12 +56,11 @@ class ShoppingListDetailsViewModel @Inject constructor(
         _deletedGroceryEvent.value = Event(Unit)
     }
 
-    private suspend fun getGroceries(shoppingListId: String) : List<Grocery>? {
-        val result = fetchGroceries(shoppingListId)
+    private fun <T>handleResult(result: Result<T>): T? {
         if (result is Result.Success) {
             return result.data
         }
-        println(result)
+        println(result) //Log the error
         return null
     }
 
@@ -60,6 +72,15 @@ class ShoppingListDetailsViewModel @Inject constructor(
     = withContext(Dispatchers.IO) {
         return@withContext try {
             Result.Success(groceryDao.getGroceriesByShoppingListId(shoppingListId))
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+    }
+
+    private suspend fun fetchShoppingList(shoppingListId: String): Result<ShoppingList>
+    = withContext(Dispatchers.IO) {
+        return@withContext try {
+            Result.Success(shoppingListDao.getShoppingListById(shoppingListId))
         } catch (e: Exception) {
             Result.Error(e)
         }
